91a17bd84138ad3e0aa7526273dd98af87b557da
Shubham-design-rgb <75521215+Shubham-design-rgb@users.noreply.github.com>
2025-09-25 18:57:02 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react_login_page",
   server: {
     host: true, // Allow access from mobile devices on same network
     port: 3000
